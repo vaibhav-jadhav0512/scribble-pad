2b2fb18499f84c20509f5d4039ff954f929649ca
Vaibhav Jadhav <vaibhav.jadhav@idrive.com>
2022-12-12 23:38:05 +0530

    Navbar highlighter implemented and usecontext implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Users from "./components/Users";
+import NoteState from "./context/notes/NoteState";
 
-function App() {
-  return <div className="App">test</div>;
-}
+const App = () => {
+  const [progress, setProgress] = useState(0);
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <LoadingBar color="#f11946" progress={progress} height={3} />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<Home setProgress={setProgress} />}
+            ></Route>
+            <Route
+              exact
+              path="/about"
+              element={<About setProgress={setProgress} />}
+            ></Route>
+            <Route
+              exact
+              path="/users"
+              element={<Users setProgress={setProgress} />}
+            ></Route>
+          </Routes>
+        </Router>
+      </NoteState>
+    </>
+  );
+};
 
 export default App;
---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,10 @@
+import React from "react";
+import noteContext from "./noteContext";
+
+const NoteState = (props) => {
+  return (
+    <noteContext.Provider value={{}}>{props.children}</noteContext.Provider>
+  );
+};
+
+export default NoteState;
